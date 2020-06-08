a67bed4b1db1cbd26c51f93aaddeaf9d5ccad03f
Unnikrishnan <uknamboodiri@gmail.com>
2020-06-08 15:52:15 +0530

    scraper

--- scrape.py
+++ scrape.py
@@ -9,21 +9,22 @@ links = soup.select('.storylink')
 subtext = soup.select('.subtext')
 
 
-def get_top_titles(news):
+def get_highest_news(news):
     return sorted(news, key=lambda k: k['points'], reverse=True)
 
 
-def create_n_dict(links, subtext):
+def create_hn_dictionary(links, subtext):
     h_news = []
     for idx, item in enumerate(links):
-        title = links[idx].getText()
-        href = links[idx].get('href')
+        title = item.getText()
+        href = item.get('href')
         vote = subtext[idx].select('.score')
         if len(vote):
-            points = int(vote[0].getText().replace(' points', ''))
+            points = int(vote[0].getText('.score').replace(' points', ''))
             if points > 99:
                 h_news.append({'title': title, 'href': href, 'points': points})
-    return get_top_titles(h_news)
+
+    return get_highest_news(h_news)
 
 
-pprint.pprint(create_n_dict(links, subtext))
+pprint.pprint(create_hn_dictionary(links, subtext))
